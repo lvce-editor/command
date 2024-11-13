130a2e892b3de9ef63b479efcfa9ace27f4f35c4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-13 21:37:26 +0100

    feature: add more tests (#5)

--- test/Command.test.ts
+++ test/Command.test.ts
@@ -1,6 +1,20 @@
 import { expect, test } from '@jest/globals'
 import * as Command from '../src/parts/Command/Command.ts'
+import * as CommandState from '../src/parts/CommandState/CommandState.ts'
+
+test('execute - error - command not found', () => {
+  expect(() => Command.execute('not-found')).toThrow(
+    new Error(`command not found not-found`),
+  )
+})
 
 test('execute', () => {
-  expect(typeof Command.execute).toBe('function')
+  const fn = () => {
+    return 2
+  }
+  const commandMap = {
+    fn,
+  }
+  CommandState.register(commandMap)
+  expect(Command.execute('fn')).toBe(2)
 })
