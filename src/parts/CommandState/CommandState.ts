0485bb31d0155da5c11aff7e8af51ff10394fc9a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-13 21:38:47 +0100

    feature: simplify code (#6)

--- src/parts/CommandState/CommandState.ts
+++ src/parts/CommandState/CommandState.ts
@@ -1,13 +1,7 @@
 const commands = Object.create(null)
 
-const registerCommand = (key: string, fn: any) => {
-  commands[key] = fn
-}
-
 export const register = (commandMap: any) => {
-  for (const [key, value] of Object.entries(commandMap)) {
-    registerCommand(key, value)
-  }
+  Object.assign(commands, commandMap)
 }
 
 export const getCommand = (key: string) => {
